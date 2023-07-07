045a3ac9578f9085cc43aab74e663278e35b0f45
allentsangdev <allentsang04@gmail.com>
2023-07-07 15:25:50 -0400

    add the input by highlighting feature

--- client/ai-text-detector-client/src/chromeServices/DOMEvaluator.ts
+++ client/ai-text-detector-client/src/chromeServices/DOMEvaluator.ts
@@ -1,3 +1,7 @@
+//Content scripts are files that run in the context of web pages. 
+//By using the standard Document Object Model (DOM), they are able to read details of the web pages
+//the browser visits, make changes to them, and pass information to their parent extension.
+
 import { DOMMessage, DOMMessageResponse } from '../types';
 
 document.addEventListener('mouseup', function(event: MouseEvent) {
--- client/ai-text-detector-client/src/components/TextInputForm.tsx
+++ client/ai-text-detector-client/src/components/TextInputForm.tsx
@@ -3,15 +3,33 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Alert from 'react-bootstrap/Alert';
+import { useState } from 'react';
 
 function TextInputForm() {
+    const [highlighedText, setHighlighedText] = useState(null)
+    
+    document.addEventListener('mouseup', function(event: MouseEvent) {
+        var selectedText: string = window.getSelection()?.toString() || '';
+        if (selectedText !== '') {
+            chrome.runtime.sendMessage({ text: selectedText });
+        }
+      })
+    
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.text) {
+          var selectedText = request.text;
+          // Do something with the selected text from the active tab
+          setHighlighedText(selectedText);
+        }
+      });
+    
     return (
         <>
             <Form className='d-grid gap-3'>
 
                 <InputGroup>
                     <FloatingLabel controlId="floatingTextarea2" label="Text">
-                        <Form.Control as="textarea" aria-label="With textarea" />
+                        {highlighedText ? <Form.Control as="textarea" aria-label="With textarea" value = {highlighedText} /> : <Form.Control as="textarea" aria-label="With textarea" /> }
                     </FloatingLabel>
                 </InputGroup>
 
