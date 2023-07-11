0b9ed74b2b44e16ce21bff7a9500286a4a2414e9
allentsangdev <allentsang04@gmail.com>
2023-07-10 22:19:40 -0400

    server setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const router = express.Router()
+const PORT = process.env.port || 4000
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+/* ----- Define Routes ----- */
+
+// Landing Route
+router.get("/", (req,res) => {
+    res.send("<h1>Server On!!!</h1>")
+})
