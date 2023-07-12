36caa25ecbf85c30b89244d4813fb9b5f4d54d82
allentsangdev <allentsang04@gmail.com>
2023-07-11 20:35:46 -0400

    update the writer ai service

--- server/modules/writer_ai_service.js
+++ server/modules/writer_ai_service.js
@@ -2,11 +2,10 @@ const axios = require('axios');
 //const writerApiKey = process.env.WRITER_API_KEY
 //const organizationId = process.env.WRITER_ORG_ID
 //const teamId = process.env.WRITER_TEAM_ID
-const writerApiKey = '59j1lbS_0Iehk3Z84KMzu1qHAGJi022wRKYa999AAdCXASw5X1aWJkXzmIbqrWBKBKuWfLG1OnPVXPrvUYEu8DdycubJuNyZeTYmdriKb1O8rypWgpfIeHyhkg27v8Va'
-const organizationId = '568358'
-const teamId = '575153'
-const content = 'this is a testing content'
-
+//const writerApiKey = '59j1lbS_0Iehk3Z84KMzu1qHAGJi022wRKYa999AAdCXASw5X1aWJkXzmIbqrWBKBKuWfLG1OnPVXPrvUYEu8DdycubJuNyZeTYmdriKb1O8rypWgpfIeHyhkg27v8Va'
+//const organizationId = '568358'
+//const teamId = '575153'
+//const content = 'this is a testing content'
 
 const writer_content_detect = (writerApiKey, organizationId, content) => {
 
@@ -25,9 +24,11 @@ const writer_content_detect = (writerApiKey, organizationId, content) => {
       .request(options)
       .then(function (response) {
         console.log(response.data);
+        return response.data
       })
       .catch(function (error) {
         console.error(error.message);
+        return error.message
       });
 
 }
@@ -36,4 +37,4 @@ module.exports = {
     writer_content_detect
 }
 
-writer_content_detect(writerApiKey, organizationId, teamId, content)
+//writer_content_detect(writerApiKey, organizationId, teamId, content)
