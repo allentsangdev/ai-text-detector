b92ff8fbcdc4e707d35c4c13051fe1c00e5a65a2
allentsangdev <allentsang04@gmail.com>
2023-07-11 20:06:50 -0400

    update express server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { writer_content_detect } = require('./modules/writer_ai_service')
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -18,15 +19,14 @@ router.get("/", (req,res) => {
 // POST Request: Writer Content Detect
 router.post('/writer/content-detect', async (req,res) => {
     try {
-        
-
+        const {writerApiKey, organzationId, content } = req.body
+        const result = await writer_content_detect(writerApiKey, organizationId, content)
+        res.status(200).json(result)
     } 
     catch(error) {
         res.status(500).send(error.message)
     }
 })
-
-
 
 app.use('/', router)
 
