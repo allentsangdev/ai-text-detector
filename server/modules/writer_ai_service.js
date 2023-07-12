84182db2e5edad07c3f9a6bd3a6d77c87d1ebb24
allentsangdev <allentsang04@gmail.com>
2023-07-11 20:00:22 -0400

    update the writer content detect api call logic

--- server/modules/writer_ai_service.js
+++ server/modules/writer_ai_service.js
@@ -1,52 +1,39 @@
 const axios = require('axios');
-const writerApiKey = process.env.WRITER_API_KEY
-const organizationId = process.env.WRITER_ORG_ID
-const teamId = process.env.WRITER_TEAM_ID
+//const writerApiKey = process.env.WRITER_API_KEY
+//const organizationId = process.env.WRITER_ORG_ID
+//const teamId = process.env.WRITER_TEAM_ID
+const writerApiKey = '59j1lbS_0Iehk3Z84KMzu1qHAGJi022wRKYa999AAdCXASw5X1aWJkXzmIbqrWBKBKuWfLG1OnPVXPrvUYEu8DdycubJuNyZeTYmdriKb1O8rypWgpfIeHyhkg27v8Va'
+const organizationId = '568358'
+const teamId = '575153'
+const content = 'this is a testing content'
 
-const writer_content_detect = () => {
+
+const writer_content_detect = (writerApiKey, organizationId, content) => {
 
     const options = {
-        method: 'POST',
-        url: `https://enterprise-api.writer.com/content/organization/${organizationId}/team/${teamId}/check`,
-        headers: {
-          accept: 'application/json',
-          authorization: {writerApiKey},
-          'content-type': 'application/json'
-        },
-        data: {
-          settings: {
-            passiveVoice: true,
-            wordiness: true,
-            unclearReference: true,
-            genderInclusivePronouns: true,
-            genderInclusiveNouns: true,
-            ageAndFamilyStatus: true,
-            disability: true,
-            genderIdentitySensitivity: true,
-            raceEthnicityNationalitySensitivity: true,
-            sexualOrientationSensitivity: true,
-            substanceUseSensitivity: true,
-            confidence: true,
-            healthyCommunication: true,
-            grammar: true,
-            spelling: true,
-            contentSafeguards: true
-          }
-        }
-      };
-      
-      axios
-        .request(options)
-        .then(function (response) {
-          console.log(response.data);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-
+      method: 'POST',
+      url: `https://enterprise-api.writer.com/content/organization/${organizationId}/detect`,
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${writerApiKey}`,
+        'content-type': 'application/json'
+      },
+      data: {input: content}
+    };
+    
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error.message);
+      });
 
 }
 
 module.exports = {
     writer_content_detect
 }
+
+writer_content_detect(writerApiKey, organizationId, teamId, content)
