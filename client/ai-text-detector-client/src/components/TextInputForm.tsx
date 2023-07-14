c257b88927b95484c2197227fb59bd41b510332f
allentsangdev <allentsang04@gmail.com>
2023-07-13 23:55:14 -0400

    updating from layout

--- client/ai-text-detector-client/src/components/TextInputForm.tsx
+++ client/ai-text-detector-client/src/components/TextInputForm.tsx
@@ -1,12 +1,21 @@
+import axios from 'axios'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Alert from 'react-bootstrap/Alert';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function TextInputForm() {
     const [highlighedText, setHighlighedText] = useState('')
+    const [checkResult, setCheckResult] = useState()
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
     
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if (request.text) {
@@ -20,17 +29,43 @@ function TextInputForm() {
         const newValue = event.target.value;
         setHighlighedText(newValue)
     }
+
+    /*async function handleSubmit() {
+        const options = {
+            method: 'POST',
+            url: `https://ai-text-detector-api.onrender.com`,
+            data: {
+                writerApiKey: writerApiKey,
+                organizationId: organizationId,
+                content: highlighedText
+            }
+            
+          };
+        const response = await axios.request(options)
+        setCheckResult(response.data)
+    }*/
     
     return (
         <>
             <Form className='d-grid gap-3'>
 
                 <InputGroup>
+                    <FloatingLabel controlId="floatingTextarea2" label="Writer API Key">
+                        <Form.Control type="password" {...register('writerApiKey')} />
+                    </FloatingLabel>
+                </InputGroup>
+
+                <InputGroup>
+                    <FloatingLabel controlId="floatingTextarea2" label="Organization ID">
+                    <Form.Control {...register('organizationId')} />
+                    </FloatingLabel>
+                </InputGroup>
+                
+                <InputGroup>
                     <FloatingLabel controlId="floatingTextarea2" label="Text">
                         <Form.Control as="textarea" aria-label="With textarea" onChange={handleChange} value = {highlighedText} />
                     </FloatingLabel>
                 </InputGroup>
-
 
                 <Button variant="primary" type="submit">
                     Submit
@@ -45,7 +80,7 @@ function TextInputForm() {
 
             <Alert variant={'dark'} style={{ marginTop: '40px' }}>
 
-                The classifier considers the text to be unclear if it is AI-generated.
+                {checkResult? `There is ${checkResult}% chance that that ocntent is human-generated content` : `The checking result will appears here`  }
 
             </Alert>
         </>
